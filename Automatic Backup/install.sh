5cec15c4257cddb44c625c6100e972a3f2f43e75
T4KUUY4 <54855101+T4KUUY4@users.noreply.github.com>
2022-09-08 22:17:42 +0200

    Create install.sh

---
+++ Automatic Backup/install.sh
@@ -0,0 +1,5 @@
+ECHO "Takuyas Auto Backup Installer"
+cd /home/$USER/ 
+wget https://raw.githubusercontent.com/T4KUUY4/Voron-Stuff/main/Automatic%20Backup/backup.sh
+wget https://raw.githubusercontent.com/T4KUUY4/Voron-Stuff/main/Automatic%20Backup/.smbcredentials
+
