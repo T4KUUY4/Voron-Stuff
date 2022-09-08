68b32261aefa0fdb22b00dd48775fb352afdd333
T4KUUY4 <54855101+T4KUUY4@users.noreply.github.com>
2022-09-08 22:18:00 +0200

    Update backup.sh

--- Automatic Backup/backup.sh
+++ Automatic Backup/backup.sh
@@ -1,29 +1,29 @@
 # Takuya´s Backup Script for Auto Backup
 
 #Replace "pi" if you have a different user. "pi" is standard in most installations.
-USER="pi" 
+USERNAME="pi" 
 
 
 # Uncomment by removing # for every backup you want to create. klipper_config is on by default
 
 # Klipper config
-cp -r /home/$USER/klipper_config /mnt/backup 
+cp -r /home/$USERNAME/klipper_config /mnt/backup 
 
 # Klipper (includes all scripts)
-#cp -r /home/$USER/klipper /mnt/backup
+#cp -r /home/$USERNAME/klipper /mnt/backup
 
 # Klipper Extras (ERCF Scripts, etc..)
 
-#cp -r /home/$USER/klipper/klippy/extas /mnt/backup
+#cp -r /home/$USERNAME/klipper/klippy/extas /mnt/backup
 
 # Klipper logs
-#cp -r /home/$USER/klipper_logs /mnt/backup
+#cp -r /home/$USERNAME/klipper_logs /mnt/backup
 
 
 # Moonraker and Moonraker-env
-#cp -r /home/$USER/moon* /mnt/backup
+#cp -r /home/$USERNAME/moon* /mnt/backup
 
 # Gcode Files
-#cp -r /home/$USER/gcode_files /mnt/backup
+#cp -r /home/$USERNAME/gcode_files /mnt/backup
 
 #Made by Takuya#0523 ;)
