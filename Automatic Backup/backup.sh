2fce12bf07768537530d7bc58efca9066700e801
T4KUUY4 <54855101+T4KUUY4@users.noreply.github.com>
2022-09-09 11:14:57 +0200

    Update backup.sh

--- Automatic Backup/backup.sh
+++ Automatic Backup/backup.sh
@@ -1,8 +1,6 @@
+#!/bin/bash
 # Takuya´s Backup Script for Auto Backup
 # On Every reboot, selected files will be automatically backuped into timestamped folders.
-
-CURRENTDATE=`date` #DONT EDIT
-MNTPATH=/mnt/backup/$CURRENTDATE #DONT EDIT
 
 ###############
 ## EDIT ZONE ##
@@ -11,26 +9,67 @@ MNTPATH=/mnt/backup/$CURRENTDATE #DONT EDIT
 #Replace "pi" if you have a different user. "pi" is standard in most installations.
 USERNAME="pi" 
 
-# Uncomment by removing # for every backup you want to create. klipper_config is on by default
+# Set to 1 to enable
+# Set to 0 to disable
 
-# Klipper config
-cp -r /home/$USERNAME/klipper_config $MNTPATH 
+#Klipper Config (default on)
+CONFIG=1
 
-# Klipper (includes all scripts)
-#cp -r /home/$USERNAME/klipper $MNTPATH
+#Klipper
+KLIPPER=0
 
-# Klipper Extras (ERCF Scripts, etc..)
+#Extras (ERCF Scripts, etc. )
+EXTRAS=0
 
-#cp -r /home/$USERNAME/klipper/klippy/extas $MNTPATH
+#LOGS
+LOGS=0
 
-# Klipper logs
-#cp -r /home/$USERNAME/klipper_logs $MNTPATH
+#Moonraker and Env
+MOONRAKER=0
+
+#G-Code Files. Warning: Will take up a lot of space over time
+GCODES=0
+
+###############
+## DON'T EDIT ZONE ##
+###############
 
 
-# Moonraker and Moonraker-env
-#cp -r /home/$USERNAME/moon* $MNTPATH
+CURRENTDATE=$(date)
+MNTPATH=/mnt/backup/$CURRENTDATE 
 
-# Gcode Files. Warning, this might take up a lot of space. 
-#cp -r /home/$USERNAME/gcode_files $MNTPATH
+if [ $CONFIG = 1 ]
+then
+  cp -r /home/$USERNAME/klipper_config "$MNTPATH"
+fi
+
+if [ $KLIPPER = 1 ]
+then
+  cp -r /home/$USERNAME/klipper "$MNTPATH"
+fi
+
+if [ $EXTRAS = 1 ]
+then
+  cp -r /home/$USERNAME/klipper/klippy/extas "$MNTPATH"
+fi
+
+if [ $LOGS = 1 ]
+then
+  cp -r /home/$USERNAME/klipper_logs "$MNTPATH"
+
+fi
+
+if [ $MOONRAKER = 1 ]
+then
+  cp -r /home/$USERNAME/moon* "$MNTPATH"
+
+fi
+
+if [ $GCODES = 1 ]
+then
+  cp -r /home/$USERNAME/gcode_files "$MNTPATH"
+
+fi
+
 
 #Made by Takuya#0523 ;)
