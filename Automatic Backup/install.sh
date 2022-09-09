ccd9241b194b5184631291cc089ada1c36d509c9
T4KUUY4 <54855101+T4KUUY4@users.noreply.github.com>
2022-09-09 14:22:07 +0200

    Update install.sh

--- Automatic Backup/install.sh
+++ Automatic Backup/install.sh
@@ -18,12 +18,14 @@ echo "sudo bash /home/$USER/backup.sh" | sudo tee -a /etc/rc.local
 clear
 
 read -p "Enable Klipper Backup? (yes/no): " klipp
+read -p "Enable Moonraker History (print stats)? (yes/no): " hist
 read -p "Enable Extras Backup (.py Scripts/ Ercf...) (yes/no): " extr
 read -p "Enable Log Backup (yes/no): " log
 read -p "Enable Moonraker Backup (yes/no): " moonrake
 read -p "Enable G-Code File Backup. Warning: Will take up more space over time (yes/no): " gcod
 
 sed -i "s/KLIPPER='no'/KLIPPER='$klipp'/" /home/$USER/backup.sh
+sed -i "s/HISTORY='no'/HISTORY='$hist'/" /home/$USER/backup.sh
 sed -i "s/EXTRAS='no'/EXTRAS='$extr'/" /home/$USER/backup.sh
 sed -i "s/LOGS='no'/LOGS='$log'/" /home/$USER/backup.sh
 sed -i "s/MOONRAKER='no'/MOONRAKER='$moonrake'/" /home/$USER/backup.sh
